cf0422e6506cd48b928fd98e6b51de76da0f28a3
forevryonenow <dimoncho.stroncho@mail.ru>
2025-06-21 23:13:09 +0400

    Add files via upload

---
+++ src/components/ButtonLeftLink.vue
@@ -0,0 +1,61 @@
+<script>
+
+</script>
+
+<template>
+    <div class="main">
+        <div class="name">
+            <img src="../assets/icon round.png" alt="иконка">
+        </div>
+        <h1>Безопастность труда</h1>
+        <div class="link">
+            <h2>Открыть</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .main{
+        background-color: rgba(255, 255, 255, 1);
+        width: 452px;
+        height: 298px;
+        border-radius: 48px;
+        padding: 25px;
+    }
+
+    .name{
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        margin-bottom: 25px;
+    }
+
+    .name h1{
+        margin-left: 15px;
+    }
+
+    .link{
+        background-color: rgba(0, 176, 240, 1);;
+        width: 145px;
+        height: 56px;
+        border-radius: 24px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 130px;
+        margin-left: 300px;
+    }
+
+    .link h2{
+        font-size: 20px;
+        margin-right: 10px;
+    }
+
+    a, h1{
+        color: rgba(5, 56, 104, 1);;
+    }
+    
+    h2{
+        color: rgba(255, 255, 255, 1);
+    }
+</style>
---
+++ src/components/HeaderbarPrime.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+    <h1>Нормативные документы</h1>
+</template>
+
+<style scoped>
+    h1{
+        color: rgba(5, 56, 104, 1);
+        font-size: 48px;
+    }
+</style>
